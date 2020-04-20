752b4d143d0243d8d44294ed1e3869ed8348266b
Bharath Raj Kumar <bharath@incend.in>
2020-04-20 13:30:19 +0530

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Bharath  Raj kumar on 20/04/20.
+//  Copyright © 2020 Bharath Raj Kumar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
